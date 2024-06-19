bba671bc0ebf86488c3ebbcdfb1bb94dca0d9a88
Shabalin-Daniil <daniilsabalin68@gmail.com>
2024-06-19 20:26:27 +0300

    fix: пароль больше не выводится

--- src/user/schemas/user.schema.ts
+++ src/user/schemas/user.schema.ts
@@ -3,5 +3,5 @@ import * as mongoose from 'mongoose';
 export const UserSchema = new mongoose.Schema({
     name: { type: String, required: true, trim: true },
     mail: { type: String, required: true, trim: true, lowercase: true, unique: true }, 
-    password: { type: String, required: true }
+    password: { type: String, required: true, select: false }
 })
